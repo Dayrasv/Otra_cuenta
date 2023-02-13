15a2c782c18b48b8fd07fe084aea142ba8c7c8aa
DayrasvHotmail <dayrasv94@hotmail.com>
2023-02-12 14:29:01 -1200

    Primer comenatrio del repo

---
+++ prueba 1.py
@@ -0,0 +1,18 @@
+nombre=input("Cual es su nombre? ");
+print(" Hola " + nombre);
+apellido =input("Cual es su apellido? ");
+print(" Hola " + nombre + " " + apellido);
+no1=int(input("Inserte el primer numero 1: "));
+no2=int(input("Inserte el primer numero 2: "));
+no=no1+no2;
+print(f"El total es: {no}");
+no1=int(input("Inserte el primer numero 1: "));
+no2=int(input("Inserte el primer numero 2: "));
+no3=int(input("Inserte el primer numero 3: "));
+print(f"La respuesta es: {(no1+no2)*no3}");
+no_dias=int(input("Inserte el numero de dias: "));
+print(f"Son: {no_dias*24} horas");
+print(f"Son: {no_dias*24*60} minutos");
+print(f"Son: {no_dias*24*60*60} segundos");
+
+
